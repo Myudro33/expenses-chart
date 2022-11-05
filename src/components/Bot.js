b0ebc809977093e1af35488a6076568188bd02f5
Myudro33 <qanashvili.myudro@gmail.com>
2022-11-05 19:54:29 +0400

    Update Bot.js

--- src/components/Bot.js
+++ src/components/Bot.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ChartWrapper from './ChartWrapper'
+import Total from './Total'
 
 const Bot = () => {
   return (
     <div className='w-full h-[26rem] xs:my-4 md:my-0 bg-white rounded-xl p-4'>
         <h1 className='text-2xl text-[#382314] font-semibold'>Spending - Last 7 days</h1>
         <ChartWrapper/>
+        <Total/>
     </div>
   )
 }
