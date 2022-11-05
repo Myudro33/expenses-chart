178b17e967aa583ed97888d00ff94bc640733c07
Myudro33 <qanashvili.myudro@gmail.com>
2022-11-05 19:43:07 +0400

    Create Bot.js

---
+++ src/components/Bot.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ChartWrapper from './ChartWrapper'
+
+const Bot = () => {
+  return (
+    <div className='w-full h-[26rem] xs:my-4 md:my-0 bg-white rounded-xl p-4'>
+        <h1 className='text-2xl text-[#382314] font-semibold'>Spending - Last 7 days</h1>
+        <ChartWrapper/>
+    </div>
+  )
+}
+
+export default Bot
